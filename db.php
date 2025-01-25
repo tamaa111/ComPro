a8fba2b76c0dd1b3e96e78dd51a2640e16123fe1
Vendy Paulus <vendypaulus@gmail.com>
2025-01-25 22:29:37 +0700

    YOLO

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'compro';
-$username = 'root'; // Sesuaikan dengan username database Anda
-$password = ''; // Sesuaikan dengan password database Anda
+$username = 'root'; 
+$password = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
